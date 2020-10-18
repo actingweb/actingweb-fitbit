42735f67e4dbdf81e704e9d77fd1d1c27da220c6
Greger Wedel <greger@greger.io>
2020-10-18 19:49:37 +0200

    Minor fix - use external_id instead of internal id

--- src/cdf.py
+++ src/cdf.py
@@ -7,6 +7,7 @@ from cognite.client import CogniteClient
 from cognite.client.data_classes import TimeSeries
 from cognite.client.exceptions import CogniteAPIError, CogniteDuplicatedError
 
+
 class Cognite:
 
     def __init__(self, me=None, project=None, environment=None, ts_name=None, ts_ext_id=None):
@@ -20,14 +21,14 @@ class Cognite:
         self.is_ok = False
         if environment:
             if environment == "greenfield":
-                url="https://greenfield.cognitedata.com/"
+                url = "https://greenfield.cognitedata.com/"
             self.environment = environment
         else:
             url = "https://api.cognitedata.com/"
             self.environment = None
         self.api_key = self.myself.property.cdf_api_key
-        self.client = CogniteClient(api_key=self.api_key, project=project, 
-            client_name="actingweb_fitbit_v1", base_url=url, debug=True)
+        self.client = CogniteClient(api_key=self.api_key, project=project,
+                                    client_name="actingweb_fitbit_v1", base_url=url, debug=True)
         status = self.client.login.status()
         if status.logged_in:
             self.is_ok = True
@@ -42,8 +43,8 @@ class Cognite:
                 self.myself.property.timeseries_id = str(ts.id)
                 return ts.id
         try:
-            ts = self.client.time_series.create(TimeSeries(name=self.ts_name, 
-                external_id=self.ts_ext_id, unit="beats"))
+            ts = self.client.time_series.create(TimeSeries(name=self.ts_name,
+                                                           external_id=self.ts_ext_id, unit="beats"))
         except CogniteAPIError:
             self.is_ok = False
         except CogniteDuplicatedError:
@@ -56,7 +57,8 @@ class Cognite:
         if not tuples:
             return False
         try:
-            self.client.datapoints.insert(tuples, id=self.ts_id)
+            self.client.datapoints.insert(tuples, external_id=self.ts_ext_id)
         except CogniteAPIError:
-            logging.ERROR("API failure to ingest timeseries " + self.ts_id)
+            logging.ERROR("API failure to ingest timeseries " +
+                          str(self.ts_ext_id))
         return True
